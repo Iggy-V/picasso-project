cee500bf74ae7fc880b121d347d97914d6e97c0f
Joe Condie <condiej24@mail.wlu.edu>
2022-12-02 11:28:04 -0500

    Added tests for cosine

--- src/tests/EvaluatorTests.java
+++ src/tests/EvaluatorTests.java
@@ -68,6 +68,29 @@ public class EvaluatorTests {
 		}
 	}
 
-	// TODO: More tests of evaluation
+	@Test
+	public void testCosEvaluation() {
+		Cosine myTree = new Cosine(new X());
+
+		// some straightforward tests
+		assertEquals(new RGBColor(Math.cos(0.6), Math.cos(0.6), Math.cos(0.6)), myTree.evaluate(.6, -1));
+		assertEquals(new RGBColor(Math.cos(0), Math.cos(0), Math.cos(0)), myTree.evaluate(0, -1));
+		assertEquals(new RGBColor(Math.cos(-1), Math.cos(-1), Math.cos(-1)), myTree.evaluate(-1, -1));
+
+		// test the ints; remember that y's value doesn't matter
+		for (int i = -1; i <= 1; i++) {
+			assertEquals(new RGBColor(Math.cos(i), Math.cos(i), Math.cos(i)), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(Math.cos(i), Math.cos(i), Math.cos(i)), myTree.evaluate(i, i));
+		}
+
+		double[] tests = { -.7, -.00001, .000001, .5 };
+
+		for (double testVal : tests) {
+			double cosOfTestVal = Math.cos(testVal);
+			assertEquals(new RGBColor(cosOfTestVal, cosOfTestVal, cosOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(cosOfTestVal, cosOfTestVal, cosOfTestVal),
+					myTree.evaluate(testVal, testVal));
+		}
+	}
 
 }
