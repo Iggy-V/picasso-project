65364b693c0ccaabbd0ae762a3810a1da53b545b
Joe Condie <condiej24@mail.wlu.edu>
2022-12-02 14:04:32 -0500

    more addition tests

--- src/tests/EvaluatorTests.java
+++ src/tests/EvaluatorTests.java
@@ -131,7 +131,7 @@ public class EvaluatorTests {
 		
 		// test the ints
 		for(int i=-1;i<=1;i++){
-			assertEquals(new RGBColor(i+(-i), i+(-i), i+(-1)), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(i+(-i), i+(-i), i+(-i)), myTree.evaluate(i, -i));
 			assertEquals(new RGBColor(i+i, i+i, i+i), myTree.evaluate(i, i));
 		}
 
