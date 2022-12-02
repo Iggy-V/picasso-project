ecbc2e20a2fa969e69a6bc54eeca23d427ac7fbf
Ignas <volcokasi25@mail.wlu.edu>
2022-12-01 22:05:03 -0500

    fixed config and abs  file names

---
+++ src/picasso/parser/tokens/functions/AbsToken.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents absolute function token 
+ * @author Elijah "Uno" Christopher
+ *
+ */
+public class AbsToken extends FunctionToken {
+	
+	public AbsToken() {
+		super("Absolute Function Token");
+	}
+
+}
