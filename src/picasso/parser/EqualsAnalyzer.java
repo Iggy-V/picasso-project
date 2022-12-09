28ab6958fd7e6428a7f5fd4da494b74404a6a53c
condiej24 <90414433+condiej24@users.noreply.github.com>
2022-12-09 15:40:48 -0500

    Update EqualsAnalyzer.java

--- src/picasso/parser/EqualsAnalyzer.java
+++ src/picasso/parser/EqualsAnalyzer.java
@@ -1,37 +1,43 @@
 package picasso.parser;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
 import picasso.parser.language.expressions.*;
 import picasso.parser.tokens.IdentifierToken;
 import picasso.parser.tokens.Token;
-import picasso.parser.tokens.operations.EqualsToken;
 
 /**
- * Handles parsing the equals sign.
+ * Handles parsing the equals sign
  * 
  * @author Joe Condie
  * 
  */
 public class EqualsAnalyzer implements SemanticAnalyzerInterface {
 	
-	
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		
+		try {
 		tokens.pop();
-		
+			
 		ExpressionTreeNode param = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		
 		IdentifierToken t = (IdentifierToken) tokens.pop();
-		//EqualsToken t = (EqualsToken) tokens.pop();
 		String id = t.getName();
-		return new Equals(id, param);
+		if (!(id.equals("x") || id.equals("y"))) {
+			return new Equals(id, param);
+		}
+		else {
+			return null;
+		}
+		}
 		
-		
+
+		catch(ClassCastException e) {
+
+			return null;
+		}
 	}
 }
 
