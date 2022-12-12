6d2b4d79f3dbf24e52a4daa4319f60448db0c007
jackdentremont <dentremontj24@mail.wlu.edu>
2022-12-12 16:44:03 -0500

    making invert an operation

--- src/picasso/parser/ExpressionTreeGenerator.java
+++ src/picasso/parser/ExpressionTreeGenerator.java
@@ -86,8 +86,6 @@ public class ExpressionTreeGenerator {
 				postfixResult.push(token);
 			} else if (token instanceof ImageToken) {
 				postfixResult.push(token);
-			} else if (token instanceof InvertToken) {
-				postfixResult.push(token);
 			} else if (token instanceof FunctionToken) {
 				operators.push(token);
 			} else if (token instanceof OperationInterface) {
