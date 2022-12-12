3f2af8be14f1f318f354757c91a3c8e6d99882b5
Ignas <volcokasi25@mail.wlu.edu>
2022-12-12 11:48:59 -0500

    Added Exp, tan, atan. Fixed exp and exponentiate

---
+++ src/picasso/parser/tokens/functions/AtanToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the Tangent function token
+ * @author Ignas Volcokas
+ */
+public class AtanToken extends FunctionToken {
+
+	public AtanToken() {
+		super("arc tan Function Token");
+	}
+
+}
