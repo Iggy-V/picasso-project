20162c990a052bf48e672b7b37b8eb47e3c6d0c0
Joe Condie <condiej24@mail.wlu.edu>
2022-12-14 17:23:23 -0600

    Added YCrCbToRGB and RGBToYCrCb

---
+++ src/picasso/parser/tokens/functions/RGBToYCrCbToken.java
@@ -0,0 +1,14 @@
+
+package picasso.parser.tokens.functions;
+
+	/**
+	 * Represents the RgbToYCrCb function token
+	 * @author Joe Condie
+	 * 
+	 */
+	public class RGBToYCrCbToken extends FunctionToken {
+
+		public RGBToYCrCbToken() {
+			super("RGBToYCrCb Function Token");
+		}
+}
---
+++ src/picasso/parser/tokens/functions/YCrCbToRGBToken.java
@@ -0,0 +1,14 @@
+
+package picasso.parser.tokens.functions;
+
+	/**
+	 * Represents the YCrCbToRGB function token
+	 * @author Joe Condie
+	 * 
+	 */
+	public class YCrCbToRGBToken extends FunctionToken {
+
+		public YCrCbToRGBToken() {
+			super("YCrCbToRGB Function Token");
+		}
+}
