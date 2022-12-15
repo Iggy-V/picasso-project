a2fe1a8b7f13b6582f18bd371ed2a7adecb532cb
Joe Condie <condiej24@mail.wlu.edu>
2022-12-14 20:14:28 -0600

    Created and implemented both PerlinBW and PerlinColor

---
+++ src/picasso/parser/tokens/functions/PerlinColorToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+
+/**
+* Represents PerlinColor function token
+* 
+* @author Joe Condie
+*/
+public class PerlinColorToken extends FunctionToken {
+	
+	public PerlinColorToken() {
+		super("PerlinColor Function Token");
+	}
+}
